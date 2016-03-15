31c9bf5da5c658f55900a6b5d1021cf709b15169
Amirho3inf <beatbotgroup@gmail.com>
2016-03-15 20:30:40 +0330

    Create bot.lua

---
+++ plugins/bot.lua
@@ -0,0 +1,22 @@
+local function run(msg, matches)
+	if permissions(msg.from.id, msg.to.id, "lang_install") then
+		-- bot.lua --
+		set_text(LANG, 'botOn', 'bot is on :-)')
+		set_text(LANG, 'botOff', 'bot is off :-/')
+		if matches[1] == 'install' then
+			return 'installed :D'
+		elseif matches[1] == 'reload' then
+			return 'reloaded'
+		end
+	else
+		return ""
+	end
+end
+
+return {
+	patterns = {
+		'#bot (install)$',
+		'#bot (reload)$',
+	},
+	run = run
+}
