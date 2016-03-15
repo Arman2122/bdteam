57afd1b3f1f97cd01130c75ef77b9e0b7aa254b1
Amirho3inf <beatbotgroup@gmail.com>
2016-03-15 21:45:55 +0330

    Update poker.lua

--- plugins/poker.lua
+++ plugins/poker.lua
@@ -1,8 +1,19 @@
 do
+
 function run(msg, matches)
-  return "😐"
-  end
+local reply_id = msg['id']
+local text = '😐'
+if matches[1] == '😐' then
+    if not is_sudo(msg) then
+reply_msg(reply_id, text, ok_cb, false)
+end
+end 
+end
 return {
-  patterns = {"😐",},
-  run = run}
+patterns = {
+    "😐"
+},
+run = run
+}
+
 end
