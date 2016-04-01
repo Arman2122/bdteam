37dd0b362cc5ca3af536c03e06a0ea232e3558db
Amirho3inf <beatbotgroup@gmail.com>
2016-04-01 23:38:28 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-    return "Self-Bot v1 \n by BeatBot Team :) <3 \n @BeatBot_Team"
+    return "Self-Bot v2 \n by BeatBot Team :) <3 \n @BeatBot_Team"
 end 
 return {
   patterns = {
