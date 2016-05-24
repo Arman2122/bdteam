88f8f42773bd5c83d614691a596a9c721e08a2ba
amirf2020 <arminghafouri8869.8869@gmail.com>
2016-05-23 23:10:12 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -241,7 +241,7 @@ function create_config( )
      "weather",
      "webshot",
      },
-    sudo_users = {94746365,184111248},
+    sudo_users = {196560568},
     admin_users = {},
     disabled_channels = {}
   }
