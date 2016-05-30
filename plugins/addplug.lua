fc93f26a215c37b390f211a49c69128b17aee68b
Arman2122 <Arman.seyri@icloud.com>
2016-05-30 18:30:08 +0430

    Update addplug.lua

--- plugins/addplug.lua
+++ plugins/addplug.lua
@@ -18,7 +18,7 @@ return {
   description = "a Usefull plugin for sudo !",  
   usage = "A plugins to add Another plugins to the server",
   patterns = {
-    "^#addplug +(.+) (.*)$"
+    "^/#addplug +(.+) (.*)$"
   },
   run = run
 }
